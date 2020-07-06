bddf2ccc2f6309d9f52399d1301d301cffa9892b
aveWorld <texnarb228@gmail.com>
2020-07-06 14:39:53 +0300

    add logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,8 @@ import Link from 'next/link'
 
 const Navbar = () => (
     <nav className="Navbar">
-            <span className="logo">csgoprofit</span>
+            {/* <span className="logo">csgoprofit</span> */}
+            <img src="/logo.svg" alt="logo" className="logo"/>
             <ul className="Navbar-ul">
                 <li><Link href="/"><a  className="nav-link">Home</a></Link></li>
                 <li><Link href="/blog"><a  className="nav-link">Blog</a></Link></li>
