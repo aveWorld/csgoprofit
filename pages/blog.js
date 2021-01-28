95b195f5b6f718efc4a91a8d88f3fec62e0e5105
aveWorld <texnarb228@gmail.com>
2021-01-28 11:18:58 +0200

    pmr update

--- pages/blog.js
+++ pages/blog.js
@@ -8,20 +8,27 @@ const blog = () => (
   <div className="blog">
     <Head>
       <title>CS:GO Profit - Blog</title>
-      <meta
-        name="description"
-        content="All about CS:GO Investments, Contracts, Trade and more"
-      />
+      <meta name="description" content="All about CS:GO Investments, Contracts, Trade and more" />
     </Head>
     <Navbar />
     <main className="blog-wrapper">
       <Link href="/promocodes">
-        <div className="promocodes-art">
-          <h3 className="promocodes-art--h1">FREE SKINS</h3>
-          <img src="/free-skins.webp" alt="free skins" />
-        </div>
+        <a>
+          <div className="promocodes-art">
+            <h3 className="promocodes-art--h1">FREE SKINS</h3>
+            <img src="/free-skins.webp" alt="free skins" />
+          </div>
+        </a>
       </Link>
       <div className="three-art one-art">
+        <Link href="/blog/new-pmr-update">
+          <a className="article hover-orange">
+            <h3 className="article-title">New PMR capsules</h3>
+            <p className="article-subtitle">Lookout of new capsules from 2021 PMR update.</p>
+            <img src="/PMR.webp" alt="inventory" />
+            <p className="article-date">Article Jan 28, 2021</p>
+          </a>
+        </Link>
         <Link href="/blog/csgo-promocodes">
           <a className="article hover-yellow">
             <h3 className="article-title">Learn about CS GO promocodes</h3>
@@ -38,8 +45,7 @@ const blog = () => (
           <a className="article hover-yellow">
             <h3 className="article-title">Check your inventory value</h3>
             <p className="article-subtitle">
-              Check your inventory value with our new tool, also check top CS:GO
-              inventories.
+              Check your inventory value with our new tool, also check top CS:GO inventories.
             </p>
             <img src="/inventory.webp" alt="inventory" />
             <p className="article-date">Article Aug 11, 2020</p>
@@ -47,12 +53,9 @@ const blog = () => (
         </Link>
         <Link href="/blog/fracture_case_and_crash_of_shatteredWeb">
           <a className="article hover-orange">
-            <h3 className="article-title">
-              New Fracture Case And Crash of Shattered Web Case
-            </h3>
+            <h3 className="article-title">New Fracture Case And Crash of Shattered Web Case</h3>
             <p className="article-subtitle">
-              Lookout of new Fracture Case and explaining price dropping of
-              shattered web
+              Lookout of new Fracture Case and explaining price dropping of shattered web
             </p>
             <img src="/fracture_collection.webp" alt="tradeups" />
             <p className="article-date">Article Aug 7, 2020</p>
@@ -60,9 +63,7 @@ const blog = () => (
         </Link>
         <Link href="/blog/two_low_risk_and_profitable_tradeups">
           <a className="article hover-green">
-            <h3 className="article-title">
-              Two Low Risk Tradeups With High Profitability
-            </h3>
+            <h3 className="article-title">Two Low Risk Tradeups With High Profitability</h3>
             <p className="article-subtitle">
               Take a look at two low risk tradeups with profitability 120%+
             </p>
@@ -74,44 +75,28 @@ const blog = () => (
       <div className="three-art">
         <Link href="/blog/buy-stickers-from-boston2018">
           <a className="article hover-green">
-            <h3 className="article-title">
-              Buy CS:GO stickers from Boston 2018 Major!
-            </h3>
+            <h3 className="article-title">Buy CS:GO stickers from Boston 2018 Major!</h3>
             <p className="article-subtitle">
-              Article explains why you should buy stickers from Boston 2018
-              Major right now
+              Article explains why you should buy stickers from Boston 2018 Major right now
             </p>
-            <img
-              src="/boston2018.webp"
-              alt="Boston 2018 major"
-              className="case-hardened--img"
-            />
+            <img src="/boston2018.webp" alt="Boston 2018 major" className="case-hardened--img" />
             <p className="article-date">Article Jul 23, 2020</p>
           </a>
         </Link>
         <Link href="/blog/make-profit-on-shattered-web-operation">
           <a className="article hover-orange">
-            <h3 className="article-title">
-              HOW TO MAKE PROFIT ON SHATTERED WEB OPERATION
-            </h3>
+            <h3 className="article-title">HOW TO MAKE PROFIT ON SHATTERED WEB OPERATION</h3>
             <p className="article-subtitle">
-              Learn about different ways to make profit on Shattered Web
-              operation
+              Learn about different ways to make profit on Shattered Web operation
             </p>
-            <img
-              src="/shattered-web.webp"
-              alt="Shattered Web"
-              className="case-hardened--img"
-            />
+            <img src="/shattered-web.webp" alt="Shattered Web" className="case-hardened--img" />
             <p className="article-date">Article Jul 20, 2020</p>
           </a>
         </Link>
         <Link href="/blog/case-hardened-patterns">
           <a className="article hover-yellow">
             <h3 className="article-title">Case Hardened Pattern</h3>
-            <p className="article-subtitle">
-              Take a look at case hardened pattern and his forms
-            </p>
+            <p className="article-subtitle">Take a look at case hardened pattern and his forms</p>
             <img
               src="/casehardened_blue.webp"
               alt="AK47 | Case Hardened"
@@ -125,9 +110,7 @@ const blog = () => (
         <Link href="/blog/three-risky-tradeups">
           <a className="article hover-green">
             <h3 className="article-title">THREE RISKY TRADE UPS</h3>
-            <p className="article-subtitle">
-              Take a look at three risky but high reward trade ups
-            </p>
+            <p className="article-subtitle">Take a look at three risky but high reward trade ups</p>
             <img src="/tradeups.webp" alt="tradeups" />
             <p className="article-date">Article Jul 1, 2020</p>
           </a>
@@ -136,24 +119,17 @@ const blog = () => (
           <a className="article hover-yellow">
             <h3 className="article-title">BEST WEBSITES FOR CS:GO TRADING</h3>
             <p className="article-subtitle">
-              You will find out websites with lowest commission, largest
-              selection of skins and more...
+              You will find out websites with lowest commission, largest selection of skins and
+              more...
             </p>
-            <img
-              src="/websites-for-trading.webp"
-              alt="best website for trading"
-            />
+            <img src="/websites-for-trading.webp" alt="best website for trading" />
             <p className="article-date">Article Jul 1, 2020</p>
           </a>
         </Link>
         <Link href="/blog/the-top-tvelve-expensive-skins-in-csgo">
           <a className="article hover-orange">
-            <h3 className="article-title">
-              THE TOP TVELVE EXPENSIVE SKINS IN CS GO
-            </h3>
-            <p className="article-subtitle">
-              Find out the most expensive skins in CS GO
-            </p>
+            <h3 className="article-title">THE TOP TVELVE EXPENSIVE SKINS IN CS GO</h3>
+            <p className="article-subtitle">Find out the most expensive skins in CS GO</p>
             <img src="/awp_dragonlore.webp" alt="AWP | Dragon Lore" />
             <p className="article-date">Article Jul 1, 2020</p>
           </a>
---
+++ pages/blog/new-pmr-update.js
@@ -0,0 +1,38 @@
+import '../../css/blog.css';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+import Head from 'next/head';
+const PmrUpdate = () => (
+  <article className="container-blog">
+    <Head>
+      <title>New 2021 PMR CS:GO capsules update</title>
+      <meta name="description" content="Lookout of new capsules from 2021 PMR CS:GO update" />
+    </Head>
+    <Navbar />
+    <div className="container-wrapper">
+      <h1 className="align-center">New stickers from 2021 CS:GO PMR Update</h1>
+      <img src="/PMR.webp" alt="Fracture Case" className="center-img pmr-update-img" />
+      <h3 className="align-center pad">Why Shattered Web Case price dropped in price so much?</h3>
+      <p>
+        Team Stickers have been added today to celebrate the hard work of CS: GO teams in 2020. For
+        these stickers, 50% of the purchases support the teams.
+      </p>
+      <h3>As usually there are 3 type of PMR capsules:</h3>
+      <ul className="pmr-update-ul">
+        <li>2020 PMR Legends</li>
+        <li>2020 PMR Challengers</li>
+        <li>2020 PMR Contenders</li>
+      </ul>
+      <img src="/pmr_cap.webp" alt="Fracture Case" className="center-img pmr-update-img" />
+      <h3>Market situation:</h3>
+      <p>
+        This news ALREADY affected the prices of old capsules, which flew off to a steep peak and
+        lost up to -20% in some positions. And I am sure that this is far from the end: after all,
+        many investors will continue to drain old capsules in panic.
+      </p>
+    </div>
+    <Footer />
+  </article>
+);
+
+export default PmrUpdate;
